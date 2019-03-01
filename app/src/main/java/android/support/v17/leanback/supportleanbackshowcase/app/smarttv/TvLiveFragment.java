922f5f9c837908a93305d2f6c63b880a44293353
unknown <jian.xu@SH01710NBW.spreadtrum.com>
2019-03-01 17:22:08 +0800

    add update tv sources from url

--- app/src/main/java/android/support/v17/leanback/supportleanbackshowcase/app/smarttv/TvLiveFragment.java
+++ app/src/main/java/android/support/v17/leanback/supportleanbackshowcase/app/smarttv/TvLiveFragment.java
@@ -8,7 +8,6 @@ import android.support.v17.leanback.supportleanbackshowcase.R;
 import android.support.v17.leanback.supportleanbackshowcase.app.details.ShadowRowPresenterSelector;
 import android.support.v17.leanback.supportleanbackshowcase.app.media.TvMetaData;
 import android.support.v17.leanback.supportleanbackshowcase.app.media.VideoExampleActivity;
-import android.support.v17.leanback.supportleanbackshowcase.app.media.VideoExampleWithExoPlayerActivity;
 import android.support.v17.leanback.supportleanbackshowcase.cards.presenters.CardPresenterSelector;
 import android.support.v17.leanback.supportleanbackshowcase.models.Card;
 import android.support.v17.leanback.supportleanbackshowcase.models.CardRow;
@@ -23,6 +22,10 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +36,8 @@ public class TvLiveFragment extends RowsFragment {
     private final String TAG = "TvLiveFragment";
     private final ArrayObjectAdapter mRowsAdapter;
     private TvListLoader mTvListLoader;
+    private TvListOnlineLoader mTvListOnlineLoader;
+    private static String TV_SOURCE_URL = "https://raw.githubusercontent.com/hulkxu/leanback-showcase/master/app/src/main/res/raw/livetv.json";
 
     public TvLiveFragment() {
         mRowsAdapter = new ArrayObjectAdapter(new ShadowRowPresenterSelector());
@@ -66,8 +71,14 @@ public class TvLiveFragment extends RowsFragment {
             mTvListLoader.cancel(false);
             mTvListLoader = null;
         }
+        if (mTvListOnlineLoader != null) {
+            mTvListOnlineLoader.cancel(false);
+            mTvListOnlineLoader = null;
+        }
         mTvListLoader = new TvListLoader();
         mTvListLoader.execute();
+        mTvListOnlineLoader = new TvListOnlineLoader();
+        mTvListOnlineLoader.execute();
     }
 
     @Override
@@ -77,6 +88,59 @@ public class TvLiveFragment extends RowsFragment {
             mTvListLoader.cancel(false);
             mTvListLoader = null;
         }
+        if (mTvListOnlineLoader != null) {
+            mTvListOnlineLoader.cancel(false);
+            mTvListOnlineLoader = null;
+        }
+    }
+
+    private final class TvListOnlineLoader extends AsyncTask<Void, Void, List<Row>> {
+
+        @Override
+        protected List<Row> doInBackground(Void... voids) {
+            Log.d(TAG, "load data from github");
+            BufferedReader reader = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                java.net.URL url = new java.net.URL(TV_SOURCE_URL);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),
+                        "utf-8"));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                String json = sb.toString();
+                CardRow[] rows = new Gson().fromJson(json, CardRow[].class);
+                List<Row> cardListRows = new ArrayList<>();
+                for (CardRow row : rows) {
+                    cardListRows.add(createCardRow(row));
+                }
+                return cardListRows;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            } finally {
+                urlConnection.disconnect();
+                if (null != reader) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        Log.e(TAG, "JSON feed closed", e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(List<Row> result) {
+            Log.d(TAG, "load finish. update UI");
+            mRowsAdapter.clear();
+            mRowsAdapter.addAll(mRowsAdapter.size(), result);
+            getMainFragmentAdapter().getFragmentHost().notifyDataReady(getMainFragmentAdapter());
+        }
+
     }
 
     private final class TvListLoader extends AsyncTask<Void, Void, List<Row>> {
@@ -94,9 +158,11 @@ public class TvLiveFragment extends RowsFragment {
             return cardListRows;
         }
 
+
         @Override
         protected void onPostExecute(List<Row> result) {
             Log.d(TAG, "load finish. update UI");
+            mRowsAdapter.clear();
             mRowsAdapter.addAll(mRowsAdapter.size(), result);
             getMainFragmentAdapter().getFragmentHost().notifyDataReady(getMainFragmentAdapter());
         }
